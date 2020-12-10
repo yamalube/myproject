a59b573d13e63b13807d9a7f7fa3f4bdc5abdf50
yamalube <ssus36957@gmail.com>
2020-12-10 15:46:50 +0800

    20201210

---
+++ isDsExists.R
@@ -0,0 +1,13 @@
+isDsExists <-function(given){
+  dn<-data()
+  dsname<-dn$results[,"Item"]  #dsname<-dn$results[,3]
+  rst<-length(dsname[dsname==given])
+  if(rst!=0){
+    return(T)
+  }
+  else{
+    return(F)
+  }
+  
+}
+isDsExists("iris")
